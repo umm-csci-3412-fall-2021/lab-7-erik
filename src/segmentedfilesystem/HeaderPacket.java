4c30cd4b8a7a2d2d12d56639db912c90c0e641f4
Erik Rauer <eriktr22@gmail.com>
2021-11-16 10:35:49 -0600

    Create `HeaderPacket` class
    Add a  new class called `HeaderPacket` which extends the `Packet` class and represents header packets.

---
+++ src/segmentedfilesystem/HeaderPacket.java
@@ -0,0 +1,18 @@
+package segmentedfilesystem;
+
+import java.net.DatagramPacket;
+import java.util.Arrays;
+
+public class HeaderPacket extends Packet {
+
+    public HeaderPacket(DatagramPacket packet) {
+        // Parse the packet according to protocol
+        int packetLength = packet.getLength();
+        byte[] allPacketData = packet.getData();
+        this.dataLength = packetLength - 2;
+
+        this.status = allPacketData[0];
+        this.fileID = allPacketData[1];
+        this.data = Arrays.copyOfRange(allPacketData, 2, dataLength);
+    }
+}
